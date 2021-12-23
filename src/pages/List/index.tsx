c8483cf4dd3ca823d82de825a411eea27c5253e6
Rodrigo Viana <roviol@gmail.com>
2021-12-22 21:57:23 -0300

    Routes

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,135 +1,39 @@
 import React from 'react';
-import { Container, Content } from './styles';
+import { Container, Content, Filters } from './styles';
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 
 const List: React.FC = () => {
-  const options = [
-    { value: 'Rodrigo', label: 'Rodrigo' },
-    { value: 'Jéssica', label: 'Jéssica' },
+  const months = [
+    { value: 7, label: 'Julho' },
+    { value: 8, label: 'Agosto' },
+    { value: 9, label: 'Setembro' },
+  ];
+
+  const years = [
+    { value: 2019, label: 2019 },
+    { value: 2020, label: 2020 },
+    { value: 2021, label: 2021 },
   ];
 
   return (
     <Container>
       <ContentHeader title="Saídas" lineColor="#e44c4e">
-        <SelectInput options={options} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
+
+      <Filters>
+        <button type="button" className="tag-filter tag-filter-recurrent">
+          Recorrentes
+        </button>
+        <button type="button" className="tag-filter tag-filter-eventual">
+          Eventuais
+        </button>
+      </Filters>
+
       <Content>
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
-        <HistoryFinanceCard
-          tagColor="#e44c4e"
-          title="Conta de Luz"
-          subtitle="22/12/2021"
-          amount="R$ 125,50"
-        />
         <HistoryFinanceCard
           tagColor="#e44c4e"
           title="Conta de Luz"
