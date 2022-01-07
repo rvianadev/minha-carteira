97fbaba801a5b6931e4fdc09e9ca201307084a74
Rodrigo Viana <roviol@gmail.com>
2022-01-07 07:55:05 -0300

    Created input and output filters

--- src/components/SelectInput/index.tsx
+++ src/components/SelectInput/index.tsx
@@ -7,12 +7,18 @@ interface ISelectInputProps {
     value: string | number;
     label: string | number;
   }[];
+  defaultValue?: string | number;
+  onChange(event: React.ChangeEvent<HTMLSelectElement>): void | undefined;
 }
 
-const SelectInput: React.FC<ISelectInputProps> = ({ options }) => {
+const SelectInput: React.FC<ISelectInputProps> = ({
+  options,
+  onChange,
+  defaultValue,
+}) => {
   return (
     <Container>
-      <select>
+      <select onChange={onChange} defaultValue={defaultValue}>
         {options.map((option) => (
           <option key={option.value} value={option.value}>
             {option.label}
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -23,6 +23,12 @@ interface IData {
 
 const List: React.FC = () => {
   const [data, setData] = useState<IData[]>([]);
+  const [monthSelected, setMonthSelected] = useState<string>(
+    String(new Date().getMonth() + 1)
+  );
+  const [yearSelected, setYearSelected] = useState<string>(
+    String(new Date().getFullYear())
+  );
 
   const { type } = useParams();
 
@@ -39,19 +45,37 @@ const List: React.FC = () => {
   }, [type]);
 
   const months = [
+    { value: 1, label: 'Janeiro' },
+    { value: 2, label: 'Fevereiro' },
+    { value: 3, label: 'Março' },
+    { value: 4, label: 'Abril' },
+    { value: 5, label: 'Maio' },
+    { value: 6, label: 'Junho' },
     { value: 7, label: 'Julho' },
     { value: 8, label: 'Agosto' },
     { value: 9, label: 'Setembro' },
+    { value: 10, label: 'Outubro' },
+    { value: 11, label: 'Novembro' },
+    { value: 12, label: 'Dezembro' },
   ];
 
   const years = [
     { value: 2019, label: 2019 },
     { value: 2020, label: 2020 },
     { value: 2021, label: 2021 },
+    { value: 2022, label: 2022 },
   ];
 
   useEffect(() => {
-    const response = listData.map((item) => {
+    const filteredDate = listData.filter((item) => {
+      const date = new Date(item.date);
+      const month = String(date.getMonth() + 1);
+      const year = String(date.getFullYear());
+
+      return month === monthSelected && year === yearSelected;
+    });
+
+    const formattedDate = filteredDate.map((item) => {
       return {
         id: String(Math.random() * listData.length),
         description: item.description,
@@ -62,14 +86,22 @@ const List: React.FC = () => {
         tagColor: item.frequency === 'recorrente' ? '#e44c4e' : '#4e41f0',
       };
     });
-    setData(response);
-  }, [type]);
+    setData(formattedDate);
+  }, [type, monthSelected, yearSelected]);
 
   return (
     <Container>
       <ContentHeader title={title} lineColor={lineColor}>
-        <SelectInput options={months} />
-        <SelectInput options={years} />
+        <SelectInput
+          options={months}
+          onChange={(e) => setMonthSelected(e.target.value)}
+          defaultValue={monthSelected}
+        />
+        <SelectInput
+          options={years}
+          onChange={(e) => setYearSelected(e.target.value)}
+          defaultValue={yearSelected}
+        />
       </ContentHeader>
 
       <Filters>
