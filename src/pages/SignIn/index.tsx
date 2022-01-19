25d3e7baa50c1cfc43d9a944c853eb858a9dcef5
Rodrigo Viana <roviol@gmail.com>
2022-01-19 12:24:42 -0300

    Created input component for login page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import logoImg from '../../assets/logo.svg';
 
+import Input from '../../components/Input';
+
 import { Container, Logo, Form, FormTitle } from './styles';
 
 const SignIn: React.FC = () => {
@@ -12,11 +14,12 @@ const SignIn: React.FC = () => {
         <h2>Minha Carteira</h2>
       </Logo>
 
-      <Form>
+      <Form onSubmit={() => {}}>
         <FormTitle>Entrar</FormTitle>
 
-        <input type="text"></input>
-        <input type="text"></input>
+        <Input type="email" placeholder="Seu e-mail" required />
+        <Input type="password" placeholder="Sua senha" required />
+
         <button type="submit">Acessar</button>
       </Form>
     </Container>
