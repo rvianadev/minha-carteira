ecd666e5e4485b77e8cea162f8ee66d3c22d0c72
Rodrigo Viana <roviol@gmail.com>
2022-01-19 00:23:38 -0300

    Login page created

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Logo, Form, FormTitle } from './styles';
+
 const SignIn: React.FC = () => {
-  return <h1>SignIn</h1>;
+  return (
+    <Container>
+      <Logo>
+        <img src={logoImg} alt="Minha Carteira" />
+        <h2>Minha Carteira</h2>
+      </Logo>
+
+      <Form>
+        <FormTitle>Entrar</FormTitle>
+
+        <input type="text"></input>
+        <input type="text"></input>
+        <button type="submit">Acessar</button>
+      </Form>
+    </Container>
+  );
 };
 
 export default SignIn;
