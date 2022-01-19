6ef2dff954a011a3dfa6882acdaf8a715f2c1f1b
Rodrigo Viana <roviol@gmail.com>
2022-01-19 16:55:27 -0300

    Logout menu created

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -9,9 +9,20 @@ import {
 
 import logoImg from '../../assets/logo.svg';
 
-import { Container, Header, LogoImg, MenuContainer, Title } from './styles';
+import { useAuth } from '../../hooks/auth';
+
+import {
+  Container,
+  Header,
+  LogoImg,
+  MenuContainer,
+  Title,
+  MenuItemButton,
+} from './styles';
 
 const Aside: React.FC = () => {
+  const { signOut } = useAuth();
+
   return (
     <Container>
       <Header>
@@ -32,10 +43,11 @@ const Aside: React.FC = () => {
           <MdArrowDownward />
           Saídas
         </Link>
-        <Link to="#">
+
+        <MenuItemButton onClick={signOut}>
           <MdExitToApp />
           Sair
-        </Link>
+        </MenuItemButton>
       </MenuContainer>
     </Container>
   );
